f482005f463a73b6baebf0d01386eed30d1d5fde
Guilherme Chaves <guilherme.chaves66@gmail.com>
2024-02-21 15:27:45 -0300

    Update script.js

--- SnakeGame/js/script.js
+++ SnakeGame/js/script.js
@@ -7,20 +7,65 @@ const size = 30
 
 const snake = [
     { x: 200, y: 200 },
-    { x: 230, y: 200 },
-    { x: 260, y: 200 },
-    { x: 290, y: 200 }
+    { x: 230, y: 200 }
 ]
 
-const drawSnake = () => {
-    ctxGame.fillStyle = "red"
+let direction;
+let loopId;
 
-    snake.forEach((position) => {
+
+const drawSnake = () => {
+    ctxGame.fillStyle = "#ddd"
+
+    snake.forEach((position, index) => {
+
+        if (index == snake.length - 1) {
+            ctxGame.fillStyle = "blue"
+        }
         ctxGame.fillRect(position.x, position.y, size, size)
     })
 }
 
-drawSnake()
+const moveSnake = () => {
+    if (!direction) return
+
+    const head = snake[snake.length - 1]
+
+    if (direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+    }
+
+    if (direction == "left") {
+        snake.push({ x: head.x - size, y: head.y })
+    }
+
+    if (direction == "up") {
+        snake.push({ x: head.x, y: head.y - size })
+    }
+
+    if (direction == "down") {
+        snake.push({ x: head.x, y: head.y + size })
+    }
+
+    snake.shift()
+}
+
+const gameLoop = () => {
+    /* if (loopId) {
+         clearInterval(loopId)
+     }
+     */
+
+    ctxGame.clearRect(0, 0, 600, 600)
+    moveSnake()
+    drawSnake()
+
+    let loopId = setTimeout(() => {
+        gameLoop()
+    }, 300)
+}
+
+gameLoop()
 
 ctxPlacar.fillStyle = "blue"
 ctxPlacar.fillRect(150, 300, 50, 50)
